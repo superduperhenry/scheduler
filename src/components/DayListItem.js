3bf985e9501fe4c6e73250efc0f003105ce98ccc
Henry Truong <henry.trb@gmail.com>
2023-06-25 18:23:09 -0400

    add dayListItem and styling with test

---
+++ src/components/DayListItem.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+import "components/DayListItem.scss";
+import classNames from "classnames";
+
+export default function DayListItem(props) {
+  let dayListItemClass = classNames("day-list__item", {
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": props.spots === 0,
+  });
+
+  const formatSpots = (props) => {
+    if (props.spots === 0) return "no spots remaining";
+    if (props.spots === 1) return "1 spot remaining";
+    return `${props.spots} spots remaining`;
+  };
+
+  return (
+    <li className={dayListItemClass} onClick={() => props.setDay(props.name)}>
+      <h2 className="text--regular">{props.name}</h2>
+      <h3 className="text--light">{formatSpots(props)}</h3>
+    </li>
+  );
+}
