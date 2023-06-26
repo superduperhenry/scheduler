6c685bcb28cde6fc36b4f39fd4c62d3ecfbd05cd
Henry Truong <henry.trb@gmail.com>
2023-06-25 23:03:50 -0400

    add interviewerList component

---
+++ src/components/InterviewerList.js
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+
+import "components/InterviewerList.scss";
+import InterviewerListItem from "./InterviewerListItem";
+
+const interviewers = [
+  { id: 1, name: "Sylvia Palmer", avatar: "https://i.imgur.com/LpaY82x.png" },
+  { id: 2, name: "Tori Malcolm", avatar: "https://i.imgur.com/Nmx0Qxo.png" },
+  { id: 3, name: "Mildred Nazir", avatar: "https://i.imgur.com/T2WwVfS.png" },
+  { id: 4, name: "Cohana Roy", avatar: "https://i.imgur.com/FK8V841.jpg" },
+  { id: 5, name: "Sven Jones", avatar: "https://i.imgur.com/twYrpay.jpg" },
+];
+
+const InterviewerList = (props) => {
+  const [selected, setSelected] = useState(props.interviewer);
+  return (
+    <section className="interviewers">
+      <h4 className="interviewers__header text--light">Interviewer</h4>
+      <ul className="interviewers__list">
+        {interviewers.map((interviewer) => {
+          const { id, name, avatar } = interviewer;
+          return (
+            <InterviewerListItem
+              key={id}
+              id={id}
+              name={name}
+              avatar={avatar}
+              selected={selected === id}
+              setInterviewer={() => setSelected(id)}
+            />
+          );
+        })}
+      </ul>
+    </section>
+  );
+};
+
+export default InterviewerList;
