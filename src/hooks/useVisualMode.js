6924433f28761280cc29a8c19f34f1632bc5fd50
Henry Truong <henry.trb@gmail.com>
2023-07-02 18:26:36 -0400

    implement customHook useVisualMode

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,23 @@
+import { useState } from "react";
+
+export default function useVisualMode(initial) {
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
+
+  function transition(newMode, replace = false) {
+    if (replace) {
+      setHistory((prev) => [...prev.slice(0, -1)]);
+    }
+    setHistory((prev) => [...prev, newMode]);
+  }
+
+  function back() {
+    if (history.length < 2) {
+      return;
+    }
+    setHistory((prev) => [...prev.slice(0, -1)]);
+    setMode(history[history.length - 1]);
+  }
+
+  return { mode: history[history.length - 1], transition, back };
+}
