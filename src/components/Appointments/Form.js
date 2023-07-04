4c35b54a7998a20df96f72817023c20d1bb48c82
Henry Truong <henry.trb@gmail.com>
2023-07-03 20:32:32 -0400

    implement unit tests for Form component

--- src/components/Appointments/Form.js
+++ src/components/Appointments/Form.js
@@ -5,6 +5,7 @@ import InterviewerList from "../InterviewerList";
 const Form = (props) => {
   const [student, setStudent] = useState(props.student || "");
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
+  const [error, setError] = useState("");
 
   const resetForm = () => {
     setStudent("");
@@ -16,6 +17,18 @@ const Form = (props) => {
     props.onCancel();
   };
 
+  function validate() {
+    if (student === "") {
+      setError("Student name cannot be blank");
+      return;
+    }
+    if (interviewer === null) {
+      setError("Please select an interviewer");
+      return;
+    }
+    setError("");
+    props.onSave(student, interviewer);
+  }
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
@@ -27,8 +40,11 @@ const Form = (props) => {
             placeholder="Enter Student Name"
             value={student}
             onChange={(e) => setStudent(e.target.value)}
+            data-testid="student-name-input"
           />
         </form>
+        <section className="appointment__validation">{error}</section>
+
         <InterviewerList
           interviewers={props.interviewers}
           value={interviewer} // value is the current id of interviewer
@@ -40,7 +56,7 @@ const Form = (props) => {
           <Button danger onClick={() => cancel()}>
             Cancel
           </Button>
-          <Button confirm onClick={() => props.onSave(student, interviewer)}>
+          <Button confirm onClick={() => validate()}>
             Save
           </Button>
         </section>
